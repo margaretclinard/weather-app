f053be469eb8dc24844c1b538aeb66294528850e
Andy Meagher <ameagher311@gmail.com>
2015-01-28 17:30:45 -0600

    added forecast descriptions but it messed up CSS

--- app/js/main.js
+++ app/js/main.js
@@ -17,49 +17,91 @@ function getJSON(url, cb){
 
 function display(data){
   var forecastData = data.forecast.txt_forecast.forecastday;
-  var docFragment = document.createDocumentFragment();   
-  var docFragment1 = document.createDocumentFragment();
-  var docFragment2 = document.createDocumentFragment();
-  var docFragment3 = document.createDocumentFragment();
-  var docFragment4 = document.createDocumentFragment();
-  var docFragment5 = document.createDocumentFragment();
+
+  var docFragPic1 = document.createDocumentFragment();
 
   var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[0].icon_url);
-  docFragment1.appendChild(img);
+  docFragPic1.appendChild(img);
   var mainPic = document.querySelector('#featured');
-  mainPic.appendChild(docFragment1);
+  mainPic.appendChild(docFragPic1);
+
+  var docFragPic2 = document.createDocumentFragment();
 
   var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[2].icon_url);
-  docFragment2.appendChild(img);
+  docFragPic2.appendChild(img);
   var secondPic = document.querySelector('#img2');
-  secondPic.appendChild(docFragment2);
+  secondPic.appendChild(docFragPic2);
 
-   var img = document.createElement('IMG');
+  var docFragPic3 = document.createDocumentFragment();
+
+  var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[4].icon_url);
-  docFragment3.appendChild(img);
+  docFragPic3.appendChild(img);
   var thirdPic = document.querySelector('#img3');
-  thirdPic.appendChild(docFragment3);
+  thirdPic.appendChild(docFragPic3);
 
-   var img = document.createElement('IMG');
+  var docFragPic4 = document.createDocumentFragment();
+
+  var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[6].icon_url);
-  docFragment4.appendChild(img);
+  docFragPic4.appendChild(img);
   var fourthPic = document.querySelector('#img4');
-  fourthPic.appendChild(docFragment4);
+  fourthPic.appendChild(docFragPic4);
 
-   var img = document.createElement('IMG');
+  var docFragPic5 = document.createDocumentFragment();
+
+  var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[8].icon_url);
-  docFragment5.appendChild(img);
+  docFragPic5.appendChild(img);
   var fifthPic = document.querySelector('#img5');
-  fifthPic.appendChild(docFragment5);
+  fifthPic.appendChild(docFragPic5);
 
   var docFragDay1 = document.createDocumentFragment();
 
   var h6 = document.createElement('H6');
   docFragDay1.appendChild(h6);
+  var text = document.createTextNode(forecastData[0].fcttext);
+  h6.appendChild(text);
+  var day1 = document.querySelector('#dayDescrip');
+  day1.appendChild(text);
+
+  var docFragDay2 = document.createDocumentFragment();
+
+  var h6 = document.createElement('H6');
+  docFragDay2.appendChild(h6);
   var text = document.createTextNode(forecastData[2].fcttext);
   h6.appendChild(text);
+  var day2 = document.querySelector('#dayDescrip2');
+  day2.appendChild(text);
+
+  var docFragDay3 = document.createDocumentFragment();
+
+  var h6 = document.createElement('H6');
+  docFragDay3.appendChild(h6);
+  var text = document.createTextNode(forecastData[4].fcttext);
+  h6.appendChild(text);
+  var day3 = document.querySelector('#dayDescrip3');
+  day3.appendChild(text);
+
+  var docFragDay4 = document.createDocumentFragment();
+
+  var h6 = document.createElement('H6');
+  docFragDay4.appendChild(h6);
+  var text = document.createTextNode(forecastData[6].fcttext);
+  h6.appendChild(text);
+  var day4 = document.querySelector('#dayDescrip4');
+  day4.appendChild(text);
+
+  var docFragDay5 = document.createDocumentFragment();
+
+  var h6 = document.createElement('H6');
+  docFragDay5.appendChild(h6);
+  var text = document.createTextNode(forecastData[8].fcttext);
+  h6.appendChild(text);
+  var day5 = document.querySelector('#dayDescrip5');
+  day5.appendChild(text);
 
 }
 
