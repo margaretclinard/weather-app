1378af2fe15b2b9ce15ab97044716b8949ba9da7
Katye Russell <katye.russell@gmail.com>
2015-01-28 15:38:27 -0600

    Continuing with button function.

--- app/js/button.js
+++ app/js/button.js
@@ -1,7 +1,7 @@
 
 var baseURL = "https://api.wunderground.com/api/92a66d84a6f9aaa7/forecast10day/q/";
 var fileExt = ".json";
-var userZip = document.getElementById("myText");
+var userZip = document.getElementById("zipCode");
 
 
 // Button needs to respond to user's click
@@ -10,7 +10,7 @@ var userZip = document.getElementById("myText");
 
 function addZip() {
     var userZip = document.getElementById("zipCode").value;
-    document.getElementById("demo").innerHTML = userZip;
+    document.getElementById("demo").innerHTML = userZip;  /* I don't think this is going to the right place... */
 }
 
 
@@ -21,3 +21,24 @@ var changedZip = userZip.concat(fileExt);
 // User's zip (w .json) gets added to the chopped url
 
 var url = baseUrl.concat(changedZip);
+
+/*
+<html>
+<head>
+<script>
+function display()
+{
+document.getElementById("displayarea").innerHTML = document.getElementById("myinput").value;    
+document.getElementById("myinput").value = "";
+}
+</script>
+</head>
+
+<body>
+<input type="text" name="myinput" id="myinput">
+<input type="button" value="Submit" name="submit" id="submit" onClick="display()"/>
+<div id="displayarea">Sample Text. This data will disappear if you press button</div>
+</body>
+</html>
+
+*/
