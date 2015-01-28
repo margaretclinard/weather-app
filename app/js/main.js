a1e640899fe540baed04e152a21869c7d1f95501
Andy Meagher <ameagher311@gmail.com>
2015-01-28 14:37:16 -0600

    started app.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
-;(function(){
-  'use strict';
+var url = 'https://api.wunderground.com/api/2691a5c498cbd94e/forecast10day/q/37206.json';
+var data;
+getJSON(url, function(request){
+  data = request;
+});
 
-  console.log('NSS Template Loaded');
-}());
+function getJSON(url, cb){
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+
+  request.onload = function () {
+    if (this.status >= 200 && this.status < 400) {
+      cb(JSON.parse(this.response));
+    }
+  }
+
+  request.send();
+}
+
+var forecastData = data.forecast.txt_forecast.forecastday;
