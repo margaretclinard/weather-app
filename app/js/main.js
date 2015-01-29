28134813bfb77c6e57598d8b541d6e5d688b277d
Andy Meagher <ameagher311@gmail.com>
2015-01-28 21:21:50 -0600

    eliminated excess createDocFragment

--- app/js/main.js
+++ app/js/main.js
@@ -18,86 +18,68 @@ function getJSON(url, cb){
 function display(data){
   var forecastData = data.forecast.txt_forecast.forecastday;
 
-  var docFragPic1 = document.createDocumentFragment();
+  var docFrag = document.createDocumentFragment();
 
-  var img = document.createElement('IMG');
-  img.setAttribute("src", forecastData[0].icon_url);
-  docFragPic1.appendChild(img);
+  var img1 = document.createElement('IMG');
+  img1.setAttribute("src", forecastData[0].icon_url);
+  docFrag.appendChild(img1);
   var mainPic = document.querySelector('#featured');
-  mainPic.appendChild(docFragPic1);
-
-  var docFragPic2 = document.createDocumentFragment();
+  mainPic.appendChild(docFrag);
 
   var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[2].icon_url);
-  docFragPic2.appendChild(img);
+  docFrag.appendChild(img);
   var secondPic = document.querySelector('#img2');
-  secondPic.appendChild(docFragPic2);
-
-  var docFragPic3 = document.createDocumentFragment();
+  secondPic.appendChild(docFrag);
 
   var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[4].icon_url);
-  docFragPic3.appendChild(img);
+  docFrag.appendChild(img);
   var thirdPic = document.querySelector('#img3');
-  thirdPic.appendChild(docFragPic3);
-
-  var docFragPic4 = document.createDocumentFragment();
+  thirdPic.appendChild(docFrag);
 
   var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[6].icon_url);
-  docFragPic4.appendChild(img);
+  docFrag.appendChild(img);
   var fourthPic = document.querySelector('#img4');
-  fourthPic.appendChild(docFragPic4);
-
-  var docFragPic5 = document.createDocumentFragment();
+  fourthPic.appendChild(docFrag);
 
   var img = document.createElement('IMG');
   img.setAttribute("src", forecastData[8].icon_url);
-  docFragPic5.appendChild(img);
+  docFrag.appendChild(img);
   var fifthPic = document.querySelector('#img5');
-  fifthPic.appendChild(docFragPic5);
+  fifthPic.appendChild(docFrag);
 
-  var docFragDay1 = document.createDocumentFragment();
-
-  var h6 = document.createElement('H6');
-  docFragDay1.appendChild(h6);
+   var h6 = document.createElement('H6');
+  docFrag.appendChild(h6);
   var text = document.createTextNode(forecastData[0].fcttext);
   h6.appendChild(text);
   var day1 = document.querySelector('#dayDescrip');
   day1.appendChild(text);
 
-  var docFragDay2 = document.createDocumentFragment();
-
-  var h6 = document.createElement('H6');
-  docFragDay2.appendChild(h6);
+  var h6 = document.createElement('h6');
+  docFrag.appendChild(h6);
   var text = document.createTextNode(forecastData[2].fcttext);
   h6.appendChild(text);
   var day2 = document.querySelector('#dayDescrip2');
   day2.appendChild(text);
 
-  var docFragDay3 = document.createDocumentFragment();
-
   var h6 = document.createElement('H6');
-  docFragDay3.appendChild(h6);
+  docFrag.appendChild(h6);
   var text = document.createTextNode(forecastData[4].fcttext);
   h6.appendChild(text);
   var day3 = document.querySelector('#dayDescrip3');
   day3.appendChild(text);
 
-  var docFragDay4 = document.createDocumentFragment();
-
   var h6 = document.createElement('H6');
-  docFragDay4.appendChild(h6);
+  docFrag.appendChild(h6);
   var text = document.createTextNode(forecastData[6].fcttext);
   h6.appendChild(text);
   var day4 = document.querySelector('#dayDescrip4');
   day4.appendChild(text);
 
-  var docFragDay5 = document.createDocumentFragment();
-
   var h6 = document.createElement('H6');
-  docFragDay5.appendChild(h6);
+  docFrag.appendChild(h6);
   var text = document.createTextNode(forecastData[8].fcttext);
   h6.appendChild(text);
   var day5 = document.querySelector('#dayDescrip5');
